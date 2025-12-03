6bf8ab006b707142413433543e2ceff9926afa00
Claus Hetzer <claus@olemiss.edu>
2025-12-03 18:33:59 +0000

    Made detection of GSL better

--- include/NCPA/interpolation/defines.hpp
+++ include/NCPA/interpolation/defines.hpp
@@ -3,17 +3,17 @@
 #include "NCPA/defines.hpp"
 
 
-#if __has_include( "gsl/gsl_spline.h" )
-#  define NCPA_INTERPOLATION_GSL_INTERPOLATION_AVAILABLE true
-#  if GSL_MAJOR_VERSION >= 2
-#    define NCPA_INTERPOLATION_GSL_STEFFEN_SPLINE_AVAILABLE true
-#  else
-#    define NCPA_INTERPOLATION_GSL_STEFFEN_SPLINE_AVAILABLE false
-#  endif
-#else
-#  define NCPA_INTERPOLATION_GSL_INTERPOLATION_AVAILABLE  false
-#  define NCPA_INTERPOLATION_GSL_STEFFEN_SPLINE_AVAILABLE false
-#endif
+// #if __has_include( "gsl/gsl_spline.h" )
+// #  define NCPA_INTERPOLATION_GSL_INTERPOLATION_AVAILABLE true
+// #  if GSL_MAJOR_VERSION >= 2
+// #    define NCPA_INTERPOLATION_GSL_STEFFEN_SPLINE_AVAILABLE true
+// #  else
+// #    define NCPA_INTERPOLATION_GSL_STEFFEN_SPLINE_AVAILABLE false
+// #  endif
+// #else
+// #  define NCPA_INTERPOLATION_GSL_INTERPOLATION_AVAILABLE  false
+// #  define NCPA_INTERPOLATION_GSL_STEFFEN_SPLINE_AVAILABLE false
+// #endif
 
 // Convenience #defines
 #define _ENABLE_IF_INDEP_IS_REAL ENABLE_IF_REAL( INDEPTYPE )
