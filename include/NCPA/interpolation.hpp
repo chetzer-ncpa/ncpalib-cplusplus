d9de0b4769dd54986156e8da2de902858813a0ff
Claus Hetzer <claus@olemiss.edu>
2026-08-27 17:40:53 +0000

    Added arrays::vectorize, arrays::extract_indices, and set default macros for interpolation

--- include/NCPA/interpolation.hpp
+++ include/NCPA/interpolation.hpp
@@ -16,3 +16,28 @@
 #include "NCPA/interpolation/nearest_neighbor_spline_2d.hpp"
 #include "NCPA/interpolation/stratified_spline_2d.hpp"
 #include "NCPA/interpolation/types.hpp"
+
+#ifndef NCPA_INTERPOLATION_DEFAULT_1D
+#  ifdef NCPA_INTERPOLATION_GSL_INTERPOLATION_AVAILABLE
+#    ifdef NCPA_INTERPOLATION_GSL_STEFFEN_SPLINE_AVAILABLE
+#      define NCPA_INTERPOLATION_DEFAULT_1D \
+          NCPA::interpolation::interpolator_1d_type_t::GSL_STEFFEN
+#    else
+#      define NCPA_INTERPOLATION_DEFAULT_1D \
+          NCPA::interpolation::interpolator_1d_type_t::GSL_CUBIC
+#    endif
+#  else
+#    define NCPA_INTERPOLATION_DEFAULT_1D \
+        NCPA::interpolation::interpolator_1d_type_t::LANL_CUBIC
+#  endif
+#endif
+
+#ifndef NCPA_INTERPOLATION_DEFAULT_2D
+#  define NCPA_INTERPOLATION_DEFAULT_2D \
+      NCPA::interpolation::interpolator_2d_type_t::LANL_BICUBIC
+#endif
+
+#ifndef NCPA_INTERPOLATION_DEFAULT_3D
+#  define NCPA_INTERPOLATION_DEFAULT_3D \
+      NCPA::interpolation::interpolator_3d_type_t::LANL_HYBRID
+#endif
